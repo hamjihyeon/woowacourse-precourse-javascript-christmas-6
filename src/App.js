01d359730c6b0580924f289e2d12ce9237a946c1
함지현 <gkawlgus01@naver.com>
2023-11-15 19:00:31 +0900

    comment: 주석삭제

--- src/App.js
+++ src/App.js
@@ -20,27 +20,6 @@ class App {
     OutputView.printDiscountOrderPrice(price, calculate, this.day);
     OutputView.printEvent(benefitAmount);
   }
-
-  async getOrderSummary(menu) {
-
-    // Console.print('\n<할인 후 예상 결제 금액>');
-    // const discountedTotalOrderPrice = totalOrderPrice - totalDiscount;
-
-    // Console.print(`${discountedTotalOrderPrice.toLocaleString()}원`);
-
-    Console.print('\n<12월 이벤트 배지>');
-    if (totalBenefitAmount >= 5000 && totalBenefitAmount < 10000) {
-      Console.print('별');
-    } else if (totalBenefitAmount >= 10000 && totalBenefitAmount < 20000) {
-      Console.print('트리');
-    } else if (totalBenefitAmount >= 20000) {
-      Console.print('산타');
-    } else {
-      Console.print('없음');
-    }
-
-    return orderSummary.trim();
-  }
 }
 
 export default App;
